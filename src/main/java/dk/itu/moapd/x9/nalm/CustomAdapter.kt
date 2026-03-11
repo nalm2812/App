0d61e3ec82660b64cddaede835112ee1b8fae657
nalm2812 <nalm@itu.dk>
2026-03-11 12:28:40 +0100

    it compiles

--- src/main/java/dk/itu/moapd/x9/nalm/CustomAdapter.kt
+++ src/main/java/dk/itu/moapd/x9/nalm/CustomAdapter.kt
@@ -1,12 +1,12 @@
 package dk.itu.moapd.x9.nalm
 
-import android.R.attr.data
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
-
+import dk.itu.moapd.x9.nalm.databinding.ListItemTrafficReportBinding
+import androidx.recyclerview.R
 class CustomAdapter(
     private val items: List<TrafficReportModel>,
 ) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
@@ -14,15 +14,32 @@ class CustomAdapter(
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int,
-    ): RecyclerView.ViewHolder =
-        RowItemBinding
+    ): ViewHolder =
+        ListItemTrafficReportBinding
             .inflate(LayoutInflater.from(parent.context), parent, false)
             .let(::ViewHolder)
+
+
     override fun onBindViewHolder(holder: ViewHolder,
                                   position: Int) {
         Log.d("myTag", "Populate an item at position: $position")
 // Bind the view holder with the selected `DummyModel` data.
-        data[position].let(holder::bind)
+        items[position].let(holder::bind)
     }
-    override fun getItemCount() = data.size
+    override fun getItemCount() = items.size
+
+
+    class ViewHolder(private val binding: ListItemTrafficReportBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(trafficReport: TrafficReportModel) {
+            binding.trafficTitle.text = trafficReport.title
+            binding.trafficLocation.text = trafficReport.location
+            binding.trafficDate.text = trafficReport.date
+            binding.trafficReportType.text = trafficReport.reportType
+            binding.trafficSeverity.text = trafficReport.severity
+            binding.trafficDesc.text = trafficReport.desc
+        }
+    }
 }
+
+
